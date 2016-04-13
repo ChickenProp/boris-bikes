26a15c760d6be1f3e6ba8c9f8d5c6f56e8303398
Phil Hazelden <philip.hazelden@gmail.com>
2016-04-13 19:28:40 +0000

    Update default API URL.
    The heroku app disappeared long ago.

--- config.py
+++ config.py
@@ -3,14 +3,14 @@
 import os
 
 #: The URL to download the CSV data from
-url = 'http://borisapi.herokuapp.com/stations.csv'
+url = 'http://www.tfl.gov.uk/tfl/syndication/feeds/cycle-hire/livecyclehireupdates.xml'
 
 #: The base dir for data storage
 datadir = os.path.expanduser('~/boris-bike-data')
 
 #: The filename format for gzipped csv files downloaded from `url`. Relative to
 #: `datadir`; uses `datetime.datetime.strftime` %-escapes.
-filename = '%Y-%m/%Y-%m-%d/%Y-%m-%d--%H:%M:%S.csv.gz'
+filename = '%Y-%m/%Y-%m-%d/%Y-%m-%d--%H:%M:%S.xml.gz'
 
 #: Max no. of times to attempt to download `url`.
 num_request_tries = 5
